cc7716ff6a3ca89549f3f20a5aa254608afa9264
ryuya226 <ryuya226@arsaga.jp>
2021-01-23 15:11:38 +0900

    authストアにログインチェックアクションの追加
    Vueインスタンス作成時にログインチェックを行い、
    
    ページリロード時にログイン状態を維持する処理の追加

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -7,10 +7,16 @@ import App from './App.vue'
 // ストアをインポートする
 import store from './store'
 
-new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む(VueRouterのインスタンス)
-  store, // ストアを読み込む(Vuexのインスタンス)
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+const createApp = async () => {
+  await store.dispatch('auth/currentUser')
+  
+  new Vue({
+    el: '#app',
+    router, // ルーティングの定義を読み込む(VueRouterのインスタンス)
+    store, // ストアを読み込む(Vuexのインスタンス)
+    components: { App }, // ルートコンポーネントの使用を宣言する
+    template: '<App />' // ルートコンポーネントを描画する
+  })
+}
+
+createApp()
--- web/resources/js/store/auth.js
+++ web/resources/js/store/auth.js
@@ -30,6 +30,12 @@ const actions = {
   async logout (context) {
     const response = await axios.post('/api/logout')
     context.commit('setUser', null)
+  },
+  // ログインチェックアクション
+  async currentUser (context) {
+    const response = await axios.get('api/user')
+    const user = response.data || null
+    context.commit('setUser', user)
   }
 }
 
